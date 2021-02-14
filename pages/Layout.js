bccc4213aa8daa2d162c79d4c9e44fa90649426a
dho <dhojpj@gmail.com>
2021-02-14 08:11:25 -0800

    cleaned up layout callback code

--- pages/Layout.js
+++ pages/Layout.js
@@ -1,8 +1,10 @@
-import Head from "next/head";
 import { useEffect } from 'react';
+import Head from "next/head";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+
 export default function Layout(props) {
+
   function addScript(path, callback = null) {
     let script = document.createElement('script');
     script.type = 'text/javascript';
@@ -14,14 +16,12 @@ export default function Layout(props) {
   }
   
   useEffect(() => {
-    // jquery needs to load first, then the callback loads the other script
+    // jquery needs to load first, then the callback loads the other scripts
     addScript("https://code.jquery.com/jquery-3.5.1.slim.min.js", function () {
-      let scripts = ["https://cdnjs.cloudflare.com/ajax/libs/gsap/3.4.2/gsap.min.js", "https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js", "https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/debug.addIndicators.min.js", "https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/animation.gsap.min.js"];
+      let scripts = ["https://cdnjs.cloudflare.com/ajax/libs/gsap/3.4.2/gsap.min.js", "https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js", "https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/debug.addIndicators.min.js", "https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/animation.gsap.min.js", "https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js", "/assets/js/app.js"];
       scripts.forEach(item => {
         addScript(item);
       });
-      addScript("https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js");
-      addScript("/assets/js/app.js");
     })
   },[]);
 
@@ -32,21 +32,12 @@ export default function Layout(props) {
         <title>87LUX</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* <Helmet>
-
-    </Helmet> */}
       {/* HEADER */}
       <Header />
       {/* CONTENT */}
       {props.children}
       {/* FOOTER*/}
       <Footer />
-      {/* <script src='https://code.jquery.com/jquery-3.5.1.slim.min.js' integrity='sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj' crossOrigin='anonymous' type="text/javascript"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.4.2/gsap.min.js" type="text/javascript"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js" type="text/javascript"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/debug.addIndicators.min.js" type="text/javascript"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/animation.gsap.min.js" type="text/javascript"></script>
-        <script src="/assets/js/app.js" type="text/javascript"></script> */}
     </>
   );
 }
