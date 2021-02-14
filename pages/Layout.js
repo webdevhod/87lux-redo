b8d1008d156c2b29faa1a154fc5a32c175dd8df7
dho <dhojpj@gmail.com>
2021-02-14 08:29:23 -0800

    reordered scripts at bottom

--- pages/Layout.js
+++ pages/Layout.js
@@ -17,9 +17,14 @@ export default function Layout(props) {
   
   useEffect(() => {
     // jquery needs to load first, then the callback loads the other scripts
+    let scripts = ["https://cdnjs.cloudflare.com/ajax/libs/gsap/3.4.2/gsap.min.js", "https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js", "https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/debug.addIndicators.min.js", "https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/animation.gsap.min.js"];
+    scripts.forEach(item => {
+      addScript(item);
+    });
+
     addScript("https://code.jquery.com/jquery-3.5.1.slim.min.js", function () {
-      let scripts = ["https://cdnjs.cloudflare.com/ajax/libs/gsap/3.4.2/gsap.min.js", "https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js", "https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/debug.addIndicators.min.js", "https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/animation.gsap.min.js", "https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js", "/assets/js/app.js"];
-      scripts.forEach(item => {
+      let scripts2 = ["https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js", "/assets/js/app.js"];
+      scripts2.forEach(item => {
         addScript(item);
       });
     })
